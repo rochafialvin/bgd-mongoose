20765db6edc48b85e756eafaebf4ee07a367bffa
rochafialvin <rochafi.dev@gmail.com>
2019-10-14 10:33:33 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+const port = 2019
+const URL = 'mongodb://127.0.0.1:27017/bdg-mongoose'
+
+mongoose.connect(
+    URL,
+    {
+        // Menggunakan url parser yang baru
+        useNewUrlParser: true,
+        // Menggunakan method baru 'CreateIndex' untuk membuat index stiap kali kita input sebuah data
+        useCreateIndex: true,
+        // Menggunakan method baru untuk proses findOneAndUpdate()
+        useFindAndModify: true,
+        // Menggunakan engine mongoDB baru
+        useUnifiedTopology: true
+        
+    }
+)
+
+// Agar API dapat memproses json
+app.use(express.json())
+
+app.listen(port, () => { console.log(`Running at ${port}`) })
