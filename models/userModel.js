c32252aea929dd4f50a66ec2bf6e2f65b4c49f86
rochafialvin <rochafi.dev@gmail.com>
2019-10-14 11:02:53 +0700

    User Schema v1

---
+++ models/userModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        set: (val) => { val.replace(/ /g, '') }, // val = data dari user, menghapus semua spasi
+        validate(val){
+            // val = 123
+            val = parseInt(val)
+            // val = 123
+
+            // Akan bernilai true jika inputan dari user merupakan sebuah angka
+            if(!isNaN(val)){
+                throw new Error("Username harus merupakan sebuah string")
+            }
+        }
+    },
+    name: {
+        type: String,
+        required: true, // wajib di isi
+        trim: true, // menghapus spasi di awal dan akhir kalimat
+        validate(val){
+            // val = 123
+            val = parseInt(val)
+            // val = 123
+
+            // Akan bernilai true jika inputan dari user merupakan sebuah angka
+            if(!isNaN(val)){
+                throw new Error("Name harus merupakan sebuah string")
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7, // Minimal 7 karakter
+    },
+    age: {
+        type: Number,
+        set: val => parseInt(val),
+        default: 0 // Jika user tidak menginput informasi umur
+    }
+
+
+})
