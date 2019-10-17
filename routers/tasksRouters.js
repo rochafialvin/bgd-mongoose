59da8c396ce447d6cdafaf770c980db7d9fb8cba
rochafialvin <rochafi.dev@gmail.com>
2019-10-17 09:42:53 +0700

    create task

--- routers/tasksRouters.js
+++ routers/tasksRouters.js
@@ -2,17 +2,41 @@ const express = require('express')
 const router = express.Router()
 
 const Task = require('../models/taskModel')
+const User = require('../models/userModel')
 
 // CREATE TASK
-router.post('/tasks', async (req, res) => {
+router.post('/tasks/:userid', async (req, res) => {
 
     try {
-        let task = new Task(req.body)
-        let resp = await task.save()
-        res.send(resp)
+        let user = await User.findById(req.params.userid)
+        let task = new Task({
+            description: req.body.description,
+            owner: user._id
+        })
+
+        // task = {_id, description, completed}
+
+        // Simpan id task yang baru ke array tasks pada user
+        user.tasks.push(task._id)
+
+        // Simpan user dan task ke database
+        await user.save()
+        await task.save()
+
+        res.send({
+            owner: {
+                name: user.name,
+                id: user._id
+            },
+            createdTask : {
+                description: task.description,
+                id: task._id,
+                owner: task.owner
+            }
+        })
 
     } catch (error) {
-        res.send(error.message)
+        
 
     }
     
